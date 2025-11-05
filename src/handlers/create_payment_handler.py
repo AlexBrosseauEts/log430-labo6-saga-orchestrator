72fd5ebe14fcf60e67fdc5e5f1ac2114ead45bb6
Alex Brosseau <brosseau.alex@outlook.com>
2025-11-04 23:17:17 -0500

    d

--- src/handlers/create_payment_handler.py
+++ src/handlers/create_payment_handler.py
@@ -33,7 +33,6 @@ class CreatePaymentHandler(Handler):
             pid = self.order_data.get("payment_id")
             if not pid:
                 return OrderSagaState.INCREASING_STOCK
-            # adapte si tu as cancel au lieu de delete
             r = requests.delete(f"{KRAKEND}/payments-api/payments/{pid}", timeout=5)
             if r.status_code in (200, 204, 404):
                 return OrderSagaState.INCREASING_STOCK
