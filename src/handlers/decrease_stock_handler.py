762ba5145ccdd610d19a78d506f2333b9aa757a5
Alex Brosseau <brosseau.alex@outlook.com>
2025-11-04 21:59:03 -0500

    import

--- src/handlers/decrease_stock_handler.py
+++ src/handlers/decrease_stock_handler.py
@@ -3,6 +3,7 @@ Handler: decrease stock
 SPDX - License - Identifier: LGPL-3.0-or-later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
+import os
 import config
 import requests
 from handlers.handler import Handler
